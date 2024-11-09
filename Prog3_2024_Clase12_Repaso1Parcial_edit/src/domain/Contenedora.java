16e45c69e4ef5872590a1e1b03f2674b02066a08
Saul Fernandez <saul.f.m@opendeusto.es>
2024-11-09 13:41:57 +0100

    Clase Contenedora con HasMap de competiciones

---
+++ Prog3_2024_Clase12_Repaso1Parcial_edit/src/domain/Contenedora.java
@@ -0,0 +1,7 @@
+package domain;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Contenedora {
+	public static HashMap<Integer, ArrayList<Competicion>> hmCompeticiones = new HashMap<>();
+}
