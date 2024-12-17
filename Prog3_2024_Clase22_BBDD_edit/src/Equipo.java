fef580a45c553ee9065533b6389a99bdb8c706bd
Saul Fernandez <saul.f.m@opendeusto.es>
2024-12-17 12:24:05 +0100

    Clase Equipo

---
+++ Prog3_2024_Clase22_BBDD_edit/src/Equipo.java
@@ -0,0 +1,32 @@
+
+public class Equipo {
+	private int id;
+	private String nombre;
+	
+	public Equipo(int id, String nombre) {
+		super();
+		this.id = id;
+		this.nombre = nombre;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	@Override
+	public String toString() {
+		return "Equipo [id=" + id + ", nombre=" + nombre + "]";
+	}
+}
